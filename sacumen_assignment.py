61d3adb0459ef2872a5d71c5ceb3b93f9136a965
vinod kumar <vinodkumar-git>
2021-11-02 19:50:11 +0530

    add truncate msg value to length of 20

--- sacumen_assignment.py
+++ sacumen_assignment.py
@@ -28,9 +28,18 @@ def listof_key_n_values(list_item):
     value = list_item[count+1:]
     return (key, value)
 
+def truncate_msg(msg_value):
+    if len(msg_value) > 20:
+        truncate_msg =  msg_value[0 : 20] + '..'
+        # print("truncated msg:",truncate_msg)
+    else:
+        truncate_msg = msg_value
+        print("truncated msg:",truncate_msg)
+    return truncate_msg
+
 # MAIN
 #to split the str from white spaces, we need to handle msg first
-str_without_msg, msg = process_msg(string)  
+str_without_msg, msg = process_msg(string) 
 
 # Now we removed the msg, convert the str into list using "" spaces.  
 list = str_to_list(str_without_msg)
@@ -55,5 +64,14 @@ keys[0] = k[-3:]
 # converting keys and values list into one dictionary.
 d = dict(zip(keys,values))
 # print(str(d).replace(', ',',\n '))
+
+msg_value = d.get('msg')
+
+truncated_msg = truncate_msg(msg_value)
+
+# replacing the msg value
+d["msg"] = truncated_msg
+
+print(str(d).replace(', ',',\n '))
 
 
